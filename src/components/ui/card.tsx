bcb7295b267c85f70db55a30afa84e10a607641d
João <joaosl.dev@gmail.com>
2025-08-27 20:02:52 -0300

    "chore(ui): normalize filenames to lowercase (case fix)"

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,92 +1,23 @@
-import * as React from 'react';
+import clsx from 'clsx';
 
-import { cn } from '@/lib/utils';
-
-function Card({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card'
-      className={cn(
-        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-header'
-      className={cn(
-        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-title'
-      className={cn('leading-none font-semibold', className)}
-      {...props}
-    />
-  );
-}
-
-function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-description'
-      className={cn('text-muted-foreground text-sm', className)}
-      {...props}
-    />
-  );
-}
-
-function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-action'
-      className={cn(
-        'col-start-2 row-span-2 row-start-1 self-start justify-self-end',
-        className,
-      )}
-      {...props}
-    />
-  );
-}
-
-function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-content'
-      className={cn('px-6', className)}
-      {...props}
-    />
-  );
-}
-
-function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot='card-footer'
-      className={cn('flex items-center px-6 [.border-t]:pt-6', className)}
-      {...props}
-    />
-  );
-}
-
-export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
+type CardProps = React.ComponentPropsWithRef<'div'> & {
+  variant?: 'default' | 'gradient';
 };
+
+export function Card({ variant = 'default', children, ...props }: CardProps) {
+  // TODO: Apply styles to the component
+
+  return (
+    <div
+      {...props}
+      className={clsx(
+        `flex flex-col gap-6 rounded-radius shadow-lg p-6`,
+        variant === 'default'
+          ? 'bg-card'
+          : 'bg-gradient-to-br from-primary/15 to-tertiary/15 dark:bg-none dark:bg-card',
+      )}
+    >
+      {children}
+    </div>
+  );
+}
