09230735912a9712f134660c7c7112055cdb697a
João <joaosl.dev@gmail.com>
2025-09-11 18:35:57 -0300

    feat(table): Add Table component from shadcn components
    Also update barrel exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -5,23 +5,9 @@ export type { ChartConfig } from './chart';
 export { Input } from './input';
 export { Label } from './label';
 export { Button } from './button';
-export {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from './card';
-export {
-  ChartLegend,
-  ChartTooltip,
-  ChartContainer,
-  ChartLegendContent,
-  ChartStyle,
-  ChartTooltipContent,
-} from './chart';
+export * from './card';
+export * from './chart';
+export * from './table';
 
 // my
 export { InputWrapper } from './input-wrapper';
@@ -29,13 +15,4 @@ export { InputSelect } from './input-select';
 export { LoadingSpinner } from './loading-spinner';
 export { ToggleTheme } from './toggle-theme';
 export { PageHeader } from './page-header';
-export {
-  DashboardCard,
-  DashboardCardHeader,
-  DashboardCardTitle,
-  DashboardCardDescription,
-  DashboardCardAction,
-  DashboardCardContent,
-  DashboardCardFooter,
-  DashboardCardDl,
-} from './dashboard-card';
+export * from './dashboard-card';
